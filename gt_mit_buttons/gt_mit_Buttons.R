d807836267e2bea85853e82637e6d76f1b94217a
sbez01 <sarah.bez@uni-tuebingen.de>
2023-07-17 17:42:36 +0200

    mobile ready draft 2: including d&d for 8 groups

--- gt_mit_buttons/gt_mit_Buttons.R
+++ gt_mit_buttons/gt_mit_Buttons.R
@@ -4,8 +4,11 @@ library(tidyverse)
 library(readxl)
 library(readxl)
 library(gtExtras)
+library(shinyWidgets)
+library(sortable)
+
  # data <- read_csv("gt_mit_buttons/data.csv") # for interactive debugging with 
-data <- read_csv("gt_mit_buttons/data.csv")
+data <- read_csv("data.csv") # ggf. Dateipfad anpassen und Überordner mit dazu nehmen
 
 data_gt <- 
   data |> 
@@ -15,7 +18,7 @@ data_gt <-
   add_row(summarise(data,
                       across(where(is.numeric), function(x) sum(x)/20),
                       across(where(is.character), ~ "Richtig pro Aufgabe [%]"))) |> 
-  relocate(`Schüler:in`, `Richtig pro SuS [%]`) |> 
+  relocate(`Schüler_in`, `Richtig pro SuS [%]`) |> 
   gt(id = "mygt") |>
   gt_color_rows(columns = c(`Richtig pro SuS [%]`:22), 
                 domain = c(0, 100),
@@ -32,6 +35,7 @@ data_gt <-
               rows = everything(),
               missing_text = "")%>%
   opt_table_font(font = list(google_font(name = "Source Sans Pro"))) |> 
+  tab_footnote(footnote = "✓ = Aufgabe richtig; x = Aufgabe falsch") %>%
   #cols_width(columns = c(`Konjunktiv Formgleichheit`:`Konjunktiv erkennen e)`) ~ px(20)) %>% # hat keinen Einfluss auf Spaltenbreite
   cols_label(
     `Richtig pro SuS [%]` = gt::html(
@@ -268,23 +272,340 @@ ui <- fluidPage(
   # header complete width
   wellPanel(
     fluidRow(
-      h4("Header"),
-      p("text"),
+      h4("Bildung homogener Lerngruppen anhand einer Lernzielkontrolle"),
+      p("Eine Lehrerin hat eine Lernzielkontrolle durchgeführt und möchte darauf basierend binnendifferenziert unterrichten. Deshalb will sie ihre Klasse in homogene Lerngruppen anhand der Ergebnisse einteilen. Sie ist gemeinsam mit weiteren pädagogischen Fachkräften in der Klasse, sodass alle Lerngruppen gut begleitet werden können."), 
+      p("Die Lehrerin überlegt, wie sie die homogenen Gruppen auf Basis der Lernzielkontrolle einteilen soll und welche Lernziele die Gruppen verfolgen sollen."),
       actionButton("b4",
-                   "Test",
-                   icon = icon("search"))
+                   "Test - Platzhalter google",
+                   icon = icon("search"),
+                   onclick ="window.open('http://google.com', '_blank')"),
     )
   ), 
   
   # table
   fluidRow(
-    
+
     # Tabelle 
     column(5, 
            wellPanel(
+             h5("Ergebnisse der Lernzielkontrolle"),
+             em("Wenn Sie auf eine Lupe klicken, sehen Sie die Aufgabe der Lernzielkontrolle. 
+               Aus Datenschutzgründen sind die Namen der Schüler:innen nicht zu sehen. Stattdessen wurden die Schüler:innen zufällig durchnummeriert."),
              gt_output('gt')
-           ))
-  ),
+           )
+  ), 
+  # Gruppenanzahl
+  column(7, 
+         wellPanel(
+           sliderInput("number_groups",
+                       "Wie viele homogene Lerngruppen sollte die Lehrerin bilden?",
+                       1, 8, 1, 1)
+         ),
+         
+         # Drag and drop 1 Gruppe
+         conditionalPanel(condition = "input.number_groups == 1",
+                          fluidRow(column(
+                            width = 12,
+                            bucket_list(
+                              header = "Ordnen Sie die Schüler*innen den Gruppen zu!",
+                              group_name = "bucket_list_group",
+                              orientation = "horizontal",
+                              add_rank_list(
+                                text = "Noch zuzuordnen",
+                                labels = data$Schüler_in,
+                                input_id = "rank_list_10"
+                              ),
+                              add_rank_list(
+                                text = "Gruppe 1",
+                                labels = NULL,
+                                input_id = "rank_list_11"
+                              )
+                            )
+                          ))), 
+         
+         # Drag and drop 2 Gruppen
+         conditionalPanel(condition = "input.number_groups == 2",
+                          fluidRow(column(
+                            width = 12,
+                            bucket_list(
+                              header = "Ordnen Sie die Schüler*innen den Gruppen zu!",
+                              group_name = "bucket_list_group",
+                              orientation = "horizontal",
+                              add_rank_list(
+                                text = "Noch zuzuordnen",
+                                labels = data$Schüler_in,
+                                input_id = "rank_list_20"
+                              ),
+                              add_rank_list(
+                                text = "Gruppe 1",
+                                labels = NULL,
+                                input_id = "rank_list_21"
+                              ),
+                              add_rank_list(
+                                text = "Gruppe 2",
+                                labels = NULL,
+                                input_id = "rank_list_22"
+                              )
+                            )
+                          ))), 
+         
+         # Drag and drop 3 Gruppen
+         conditionalPanel(condition = "input.number_groups == 3",
+                          fluidRow(column(
+                            width = 12,
+                            bucket_list(
+                              header = "Ordnen Sie die Schüler*innen den Gruppen zu!",
+                              group_name = "bucket_list_group",
+                              orientation = "horizontal",
+                              add_rank_list(
+                                text = "Noch zuzuordnen",
+                                labels = data$Schüler_in,
+                                input_id = "rank_list_30"
+                              ),
+                              add_rank_list(
+                                text = "Gruppe 1",
+                                labels = NULL,
+                                input_id = "rank_list_31"
+                              ),
+                              add_rank_list(
+                                text = "Gruppe 2",
+                                labels = NULL,
+                                input_id = "rank_list_32"
+                              ),
+                              add_rank_list(
+                                text = "Gruppe 3",
+                                labels = NULL,
+                                input_id = "rank_list_33"
+                              )
+                            )
+                          ))), 
+         
+         # Drag and drop 4 Gruppen
+         conditionalPanel(condition = "input.number_groups == 4",
+                          fluidRow(column(
+                            width = 12,
+                            bucket_list(
+                              header = "Ordnen Sie die Schüler*innen den Gruppen zu!",
+                              group_name = "bucket_list_group",
+                              orientation = "horizontal",
+                              add_rank_list(
+                                text = "Noch zuzuordnen",
+                                labels = data$Schüler_in,
+                                input_id = "rank_list_40"
+                              ),
+                              add_rank_list(
+                                text = "Gruppe 1",
+                                labels = NULL,
+                                input_id = "rank_list_41"
+                              ),
+                              add_rank_list(
+                                text = "Gruppe 2",
+                                labels = NULL,
+                                input_id = "rank_list_42"
+                              ),
+                              add_rank_list(
+                                text = "Gruppe 3",
+                                labels = NULL,
+                                input_id = "rank_list_43"
+                              ),
+                              add_rank_list(
+                                text = "Gruppe 4",
+                                labels = NULL,
+                                input_id = "rank_list_44"
+                              )
+                            )
+                          ))), 
+         
+         
+         # Drag and drop 5 Gruppen
+         conditionalPanel(condition = "input.number_groups == 5",
+                          fluidRow(
+                            column(
+                              width = 12,
+                              bucket_list(
+                                header = "Ordnen Sie die Schüler*innen den Gruppen zu!",
+                                group_name = "bucket_list_group",
+                                orientation = "horizontal",
+                                add_rank_list(
+                                  text = "Noch zuzuordnen",
+                                  labels = data$Schüler_in,
+                                  input_id = "rank_list_50"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 1",
+                                  labels = NULL,
+                                  input_id = "rank_list_51"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 2",
+                                  labels = NULL,
+                                  input_id = "rank_list_52"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 3",
+                                  labels = NULL,
+                                  input_id = "rank_list_53"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 4",
+                                  labels = NULL,
+                                  input_id = "rank_list_54"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 5",
+                                  labels = NULL,
+                                  input_id = "rank_list_55"
+                                ))))),
+         
+         # Drag and drop 6 Gruppen
+         conditionalPanel(condition = "input.number_groups == 6",
+                          fluidRow(
+                            column(
+                              width = 12,
+                              bucket_list(
+                                header = "Ordnen Sie die Schüler*innen den Gruppen zu!",
+                                group_name = "bucket_list_group",
+                                orientation = "horizontal",
+                                add_rank_list(
+                                  text = "Noch zuzuordnen",
+                                  labels = data$Schüler_in,
+                                  input_id = "rank_list_60"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 1",
+                                  labels = NULL,
+                                  input_id = "rank_list_61"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 2",
+                                  labels = NULL,
+                                  input_id = "rank_list_62"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 3",
+                                  labels = NULL,
+                                  input_id = "rank_list_63"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 4",
+                                  labels = NULL,
+                                  input_id = "rank_list_64"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 5",
+                                  labels = NULL,
+                                  input_id = "rank_list_65"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 6",
+                                  labels = NULL,
+                                  input_id = "rank_list_66"
+                                ))))),
+         
+         
+         # Drag and drop 7 Gruppen
+         conditionalPanel(condition = "input.number_groups == 7",
+                          fluidRow(
+                            column(
+                              width = 12,
+                              bucket_list(
+                                header = "Ordnen Sie die Schüler*innen den Gruppen zu!",
+                                group_name = "bucket_list_group",
+                                orientation = "horizontal",
+                                add_rank_list(
+                                  text = "Noch zuzuordnen",
+                                  labels = data$Schüler_in,
+                                  input_id = "rank_list_70"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 1",
+                                  labels = NULL,
+                                  input_id = "rank_list_71"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 2",
+                                  labels = NULL,
+                                  input_id = "rank_list_72"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 3",
+                                  labels = NULL,
+                                  input_id = "rank_list_73"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 4",
+                                  labels = NULL,
+                                  input_id = "rank_list_74"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 5",
+                                  labels = NULL,
+                                  input_id = "rank_list_75"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 6",
+                                  labels = NULL,
+                                  input_id = "rank_list_76"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 7",
+                                  labels = NULL,
+                                  input_id = "rank_list_77"
+                                ))))), 
+         # Drag and drop 8 Gruppen
+         conditionalPanel(condition = "input.number_groups == 8",
+                          fluidRow(
+                            column(
+                              width = 12,
+                              bucket_list(
+                                header = "Ordnen Sie die Schüler*innen den Gruppen zu!",
+                                group_name = "bucket_list_group",
+                                orientation = "horizontal",
+                                add_rank_list(
+                                  text = "Noch zuzuordnen",
+                                  labels = data$Schüler_in,
+                                  input_id = "rank_list_80"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 1",
+                                  labels = NULL,
+                                  input_id = "rank_list_81"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 2",
+                                  labels = NULL,
+                                  input_id = "rank_list_82"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 3",
+                                  labels = NULL,
+                                  input_id = "rank_list_83"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 4",
+                                  labels = NULL,
+                                  input_id = "rank_list_84"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 5",
+                                  labels = NULL,
+                                  input_id = "rank_list_85"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 6",
+                                  labels = NULL,
+                                  input_id = "rank_list_86"
+                                ),
+                                add_rank_list(
+                                  text = "Gruppe 7",
+                                  labels = NULL,
+                                  input_id = "rank_list_87"
+                                ),
+                                add_rank_list( 
+                                  text = "Gruppe 8",
+                                  labels = NULL,
+                                  input_id = "rank_list_88"
+                                ))))),
   
   fluidRow(
     wellPanel(
@@ -292,7 +613,7 @@ ui <- fluidPage(
       verbatimTextOutput("debug02"),
     )
   )
-)
+)))
 
 
 server <- function(input, output, session) {
@@ -309,6 +630,43 @@ server <- function(input, output, session) {
     renderText({
       input$i1
     })
+  
+  output$results_1 <-
+    renderPrint(
+      input$rank_list_1 # This matches the input_id of the first rank list
+    )
+  output$results_2 <-
+    renderPrint(
+      input$rank_list_2 # This matches the input_id of the second rank list
+    )
+  output$results_3 <-
+    renderPrint(
+      input$bucket_list_group # Matches the group_name of the bucket list
+    )
+  
+  # test updating the bucket list label
+  counter_bucket <- reactiveVal(1)
+  observe({
+    update_bucket_list(
+      "bucket_list_group",
+      text = paste("You pressed the button", counter_bucket(), "times"),
+      session = session
+    )
+    counter_bucket(counter_bucket() + 1)
+  }) %>%
+    bindEvent(input$btnUpdateBucket)
+  
+  # test updating the rank list label
+  counter_rank <- reactiveVal(1)
+  observe({
+    update_rank_list(
+      "rank_list_1",
+      text = paste("You pressed the button", counter_rank(), "times"),
+      session = session
+    )
+    counter_rank(counter_rank() + 1)
+  }) %>%
+    bindEvent(input$btnUpdateRank)
   
 }
 
